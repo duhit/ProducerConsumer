c3010f672bf21c1080ac1ae7e4e5544e3160f0a9
Duhit Choudhary <Duhit Choudhary>
2016-03-10 14:02:49 +0000

    added two producer and consumer objects in main class

--- src/com/cloudwick/mcgeary/derwin/Main.java
+++ src/com/cloudwick/mcgeary/derwin/Main.java
@@ -4,14 +4,18 @@ public class Main {
 	
 	public static void main(String[] args) {
 		Box b = new Box();
-		Producer p = new Producer(b);
-		Consumer c = new Consumer(b);
+		Producer p1 = new Producer(b);
+		Producer p2 = new Producer(b);
+		Consumer c1 = new Consumer(b);
+		Consumer c2 = new Consumer(b);
 		
-		p.start();
-		c.start();
+		p1.start();
+		p2.start();
+		c1.start();
+		c2.start();
 		
 		try {
-			c.join();
+			c2.join();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
